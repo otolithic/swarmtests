33fb29c46a3862723dbef82d5d153897f0097704
kate <cakeflanagan@gmail.com>
2019-12-12 17:35:27 -0500

    Cleanup and organize

--- main.js
+++ main.js
@@ -19,27 +19,23 @@ var xpos = 0;
 var ypos = 0;
 var cursorX = 0;
 var cursorY = 0;
-var cursorVelX = 0;
-var cursorVelY = 0;
-var sight = 100;
-var neighbordist = 250;
-var cohesion = 0.00001;
-var repulsion = 0.01;
-var consensus = 0;
-var antsyness = 0.1;
-var cursorBufferX = Array(100).fill(0);
-var cursorBufferY = Array(100).fill(0);
 
-//create followers
-for (var i = 0; i<numSquares; i++) {
-    followers[i] = new Follower(xpos,ypos,size,i);
-}
+var sight = 100;
+var neighbordist = 100;
+var cohesion = 0.02;//0.002;
+var repulsion = 2.5; //0.5;
+var consensus = 1;
+var antsyness = 0.1;
 
 //listener
 canvas.addEventListener("mousemove",getCursorXY,false);
+function getCursorXY(e) {
+    cursorX = e.clientX;
+	cursorY = e.clientY;
+}
 
 //Follower definition
-function Follower(x,y,size,id) {
+function Follower(x,y,id) {
     //target position relative to the cursor
     this.xoffset = Math.random()*50*2-50;
     this.yoffset = Math.random()*50*2-50;
@@ -56,10 +52,8 @@ function Follower(x,y,size,id) {
     //change this to a velocity
     this.steps = Math.random()*10+100;
     
-    //size of image
-    this.s = size;
     //random color of image
-    this.color = getRandomColor();
+    this.color = 'red';
     
     this.neighbors=[];
     //this.steps = Math.random()*10+5;
@@ -75,41 +69,85 @@ function Follower(x,y,size,id) {
     this.yheading = this.yheading/hyp;
 }
 
-
-//functions
-function getCursorXY(e) {
-    cursorX = e.clientX;
-	cursorY = e.clientY;
+//create agents
+for (var i = 0; i<numSquares; i++) {
+    followers[i] = new Follower(xpos,ypos,i);
 }
 
-function drawSquares(cursorX,cursorY,followers) {
+function getDist(x1,x2,y1,y2) {
+    var xdiff = x2-x1;
+    var ydiff = y2-y1;
+    var dist = Math.hypot(xdiff,ydiff);
+    return dist;
+}
+
+//// NEIGHBORHOOD ////
+//find nearest neighbors by checking if the distance to each other agent is less than the threshold
+function getNeighbors(f) {
+    f.neighbors = [];
+    followers.forEach(function (g) {
+        if (f.id !== g.id){
+            if (getDist(f.xpos,g.xpos,f.ypos,g.ypos) < neighbordist) {
+                f.neighbors.push(g)
+                f.color='orange';
+            }
+        }
+    });
+}
+
+//// BEHAVIOR ////
+//look in the average direction of your neighbors
+function getAverageHeading(f) {
+    var totalxh=f.xheading;
+    var totalyh=f.yheading;
+    
+    //iterate through neighbors and add headings to total
+    for (var i=0; i<f.neighbors.length;i++){
+        var n = f.neighbors[i];
+        
+        // leader multiplier biases towards leader
+        var mult = 1;
+        if (n.isleader)
+            mult = numSquares;
+        
+        var proximity = getDist(f.xpos,n.xpos,f.ypos,n.ypos);
+        
+        // TODO: figure out exactly what consensus mult is
+        totalxh += mult*n.xheading*(consensus/(proximity*proximity));
+        totalyh += mult*n.yheading*(consensus/(proximity*proximity));
+        
+        //repulsion if too close
+        if (proximity<size*2) {
+            totalxh += repulsion*(f.xpos-n.xpos)/proximity;
+            totalyh += repulsion*(f.ypos-n.ypos)/proximity;
+        }
+        //cohesion otherwise
+        else {
+            totalxh -= cohesion*(f.xpos-n.xpos)/proximity;
+            totalyh -= cohesion*(f.ypos-n.ypos)/proximity;    
+        }
+    }
+    //average headings
+    f.xheading = totalxh/Math.hypot(totalxh,totalyh);
+    f.yheading = totalyh/Math.hypot(totalxh,totalyh);
+}
+
+//// UPDATE POSITIONS ////
+function drawSquares(cursorX,cursorY) {
     var rectW = size;
     var rectH = size;
     
     context.clearRect(0,0,canvas.width,canvas.height);
     
     followers.forEach(function (f) {
-        //distance from cursor is cursor position minus the current position of the follower
+        f.color = 'red';
+        getNeighbors(f)
+        
+        //// LEADERSHIP //// 
+        // assign leadership status based on proximity to cursor
+        // for some reason this doesn't work as its own function
         f.dx = cursorX - f.xpos;
         f.dy = cursorY - f.ypos;
-        
-        f.color = 'red';
-        
-        //find nearest neighbors
-        f.neighbors = [];
-        followers.forEach(function (g) {
-            if (f.id !== g.id){
-                var xdiff = f.xpos - g.xpos;
-                var ydiff = f.ypos - g.ypos;
-                var dist = Math.hypot(xdiff,ydiff);
-                if (dist < neighbordist) {
-                    f.neighbors.push(g)
-                    f.color='orange';
-                }
-            }
-        });
-        
-        //assign leadership status based on proximity to cursor
         var dist2cursor = Math.hypot(f.dx, f.dy);
         if (dist2cursor < sight) {
             f.isleader = true;
@@ -118,53 +156,31 @@ function drawSquares(cursorX,cursorY,followers) {
         else {
             f.isleader = false;
         }
-
+        
         //new position of the follower should be updated by adding a fraction of the distance between it and the cursor
         if (f.isleader) {
             f.xheading = f.dx/dist2cursor;
             f.yheading = f.dy/dist2cursor;
-            
-            f.xpos += 20*f.xheading/f.steps;
-            f.ypos += 20*f.yheading/f.steps;
         }
         else {
-            //look in the average direction of your neighbors
-            var totalxh=f.xheading;
-            var totalyh=f.yheading;
-            for (var i=0; i<f.neighbors.length;i++){
-                var n = f.neighbors[i];
-                var mult = 1;
-                if (n.isleader)
-                    mult = numSquares;
-                totalxh += mult*consensus*n.xheading;
-                totalyh += mult*consensus*n.yheading;
-                
-                if (Math.hypot(f.xpos-n.xpos,f.ypos-n.ypos)<size*2) {
-                    totalxh += repulsion*(f.xpos-n.xpos);
-                    totalyh += repulsion*(f.ypos-n.ypos);
-                }
-                else {
-                    totalxh -= cohesion*(f.xpos-n.xpos);
-                    totalyh -= cohesion*(f.ypos-n.ypos);    
-                }
-            }
-            f.xheading = totalxh/Math.hypot(totalxh,totalyh);
-            f.yheading = totalyh/Math.hypot(totalxh,totalyh);
+            getAverageHeading(f);
             
-            if (!isNaN(f.xheading) && !isNaN(f.yheading)) {
-                f.xpos += 20*f.xheading/f.steps;
-                f.ypos += 20*f.yheading/f.steps;
-            } 
-            
+            // add some random behavior to the final result
             var randhx = Math.random()*2-1;
             var randhy = Math.random()*2-1;
             
-            f.xheading = f.xheading+randhx*antsyness;
-            f.yheading = f.yheading+randhy*antsyness;
+            f.xheading += randhx*antsyness;
+            f.yheading += randhy*antsyness;
             f.xheading = f.xheading/Math.hypot(f.xheading,f.yheading);
             f.yheading = f.yheading/Math.hypot(f.xheading,f.yheading);
         }
-            
+        
+        //update position based on heading and speed
+        if (!isNaN(f.xheading) && !isNaN(f.yheading)) {
+            f.xpos += 20*f.xheading/f.steps;
+            f.ypos += 20*f.yheading/f.steps;
+        } 
+        
         if (f.xpos > canvas.width) {
             f.xpos = f.xpos % canvas.width;
         }
@@ -191,6 +207,11 @@ function animate() {
     requestAnimationFrame(animate);
 }
 
+
+//animation call
+animate();
+
+/*
 function getRandomColor() {
     var randocolor =[Math.floor(Math.random()*256), Math.floor(Math.random()*256), Math.floor(Math.random()*256)];
 
@@ -207,15 +228,10 @@ function getRandomColor() {
     }
     return hexcolor;
 }
-
-//animation call
-animate();
-
-
+*/
 /*
 
     TO DO:
-    - allow Followers to move while mouse is still while remaining within a given range of the mouse
     - avoid Follower collisions by maintaining knowledge among the entire Swarm of neighbors' locations
     - create an independent Swarm with self-guided, random position that avoids the mouse Swarm's location.
 
